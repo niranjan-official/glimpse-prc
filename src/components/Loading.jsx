936b0fa8f84345af6ad87268746504dabc1429de
niranjan-official <niranjansabarinath1521@gmail.com>
2024-07-16 16:36:55 +0530

    ui: loading screen ui update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '@/assets/prc-logo.png'
 
 const Loading = () => {
   return (
-    <div>
-      Loading
+    <div className="flex h-screen w-full items-center justify-center bg-white">
+      <img src={logo} className='h-40 md:h-1/2 w-auto' alt="" />
     </div>
   )
 }
